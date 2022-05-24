8fee1dddff6d555122e9e5d19ea8786ccde35109
RihardsV10 <rihardsvaldens@gmail.com>
2022-05-24 16:40:16 +0200

    JS

---
+++ Projekts.js
@@ -0,0 +1,8 @@
+console.log("Darbs tiek veikts Individuāli, Darba autors:RihardsV10")
+let text = "<br>";
+let result = text.repeat(37);
+document.getElementById("rep").innerHTML = result;
+
+let texT = "<br>";
+let resulT = texT.repeat(36);
+document.getElementById("rep1").innerHTML = resulT;
